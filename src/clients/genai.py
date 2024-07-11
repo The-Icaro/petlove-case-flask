a50a7a9ec641259033f9ce3126849348b19ac9c8
The-Icaro <icarofigaro100@hotmail.com>
2024-07-11 01:54:13 -0300

    feat: implement ask with file context route

--- src/clients/genai.py
+++ src/clients/genai.py
@@ -3,12 +3,14 @@ from logging import getLogger
 import google.generativeai as genai
 from flask import current_app
 
+from src.utils.consts import GEN_AI_CONTEXT, GEN_AI_MODEL
+
 logger = getLogger(__name__)
 
 class GenerativeAI:
     __slots__ = ['api_key', 'default_genai_model']
 
-    def __init__(self, genai_model: str = 'gemini-1.5-flash'):
+    def __init__(self, genai_model: str = GEN_AI_MODEL):
         self.api_key = current_app.config['GENAI_API_KEY']
         self.default_genai_model = genai_model
 
@@ -18,10 +20,23 @@ class GenerativeAI:
         logger.info("Asking question to GenAI model")
 
         model = genai.GenerativeModel()
-
-        context = "Você é um assistente de vendas do ecommerce Petlove, reponda a seguinte pergunta:"
-        response = model.generate_content(context + query)
+        response = model.generate_content(GEN_AI_CONTEXT + query)
 
         logger.info("GenAI generate content completed")
+
+        return response.text
+
+    def ask_with_file(self, query: str, file_path: str) -> str:
+        logger.info("Asking question to GenAI model with file")
+
+        model = genai.GenerativeModel()
+
+        filename = file_path.split('/')[-1]
+        uploaded_file = genai.upload_file(path=file_path, display_name=filename, mime_type="text/plain")
+
+        file = genai.get_file(name=uploaded_file.name)
+        response = model.generate_content([GEN_AI_CONTEXT + query, file])
+
+        logger.info("GenAI generate content with file completed")
 
         return response.text
--- src/config.py
+++ src/config.py
@@ -3,3 +3,6 @@ import os
 
 class Config:
     GENAI_API_KEY = os.environ.get('GENAI_API_KEY')
+    UPLOAD_FOLDER="./files"
+    MAX_CONTENT_LENGTH=16 * 1024 * 1024
+    ALLOWED_EXTENSIONS=["txt"]
